f8b5fe0c37ee82369bfc3fb76c9d7a7b2387258d
Piyush Rai <piyushraivns31@gmail.com>
2026-08-07 11:30:56 +0530

    react- springboot- built header and footer using react

---
+++ section03/eazystore-ui/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import "./footer.css";
+
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            Made with
+            <FontAwesomeIcon icon={faHeart} className="footer-icon" aria-hidden="true" />
+            by
+            <a href="https://eazybytes.com/" target="_blank" rel="noreferrer">eazybytes</a>
+        </footer>
+    );
+}
