12764bbdd0ceef02fb6857ec5ea8ffaef2547f59
Piyush Rai <piyushraivns31@gmail.com>
2026-08-30 19:43:40 +0530

    added sorting functionality based on selected options

--- section08/eazystore-ui/src/components/ProductListings.jsx
+++ section08/eazystore-ui/src/components/ProductListings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import ProductCard from "./ProductCard";
 import SearchBox from "./SearchBox";
 import DropDown from "./DropDown";
@@ -7,16 +7,61 @@ const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
 export default function ProductListings({ products }) {
   const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState("Popularity");
+  const filterAndSortProducts= useMemo(() => {
+    if (!Array.isArray(products)) {
+      return [];
+    }
 
-  const handleSearchChange = function (inputSearch) {
+    let filteredProducts = products.filter((product) =>
+      product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      product.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    //optimized way- shallow copy slice() does
+    return filteredProducts.slice().sort((a, b) => {
+      switch (selectedSort) {
+        case "Price Low to High":
+          return parseFloat(a.price) - parseFloat(b.price);
+        case "Price High to Low":
+          return parseFloat(b.price) - parseFloat(a.price);
+        case "Popularity":
+        default:
+          return parseInt(b.popularity) - parseInt(a.popularity);
+      }
+    });
+
+    // switch (selectedSort) {
+    //   case "Price Low to High":
+    //     filterAndSortProducts= filterAndSortProducts.sort(
+    //       (a, b) =>
+    //         parseInt(a.price) - parseInt(b.price)
+    //     );
+    //     break;
+    //   case "Price High to Low":
+    //     filterAndSortProducts= filterAndSortProducts.sort(
+    //       (a, b) =>
+    //         parseFloat(b.price) - parseFloat(a.price)
+    //     );
+    //     break;
+    //   default:
+    //     filterAndSortProducts= filterAndSortProducts.sort(
+    //       (a, b) =>
+    //         parseInt(b.popularity) - parseInt(a.popularity)
+    //     );
+    // }
+
+    // return filterAndSortProducts;
+
+  }, [products, searchText, selectedSort]);
+
+  function handleSearchChange(inputSearch) {
     setSearchText(inputSearch);
   }
 
-  let filterAndSortProducts = Array.isArray(products) ? (products.filter((product) =>
-    product.name.toLowerCase().includes(searchText.toLowerCase()) ||
-    product.name.toLowerCase().includes(searchText.toLowerCase())
-  ))
-    : [];
+  function handleSortChange(value) {
+    setSelectedSort(value);
+  }
 
 
   return (
@@ -31,7 +76,8 @@ export default function ProductListings({ products }) {
         <DropDown
           label="Sort By"
           options={sortList}
-          selectedValue="Popularity"
+          selectedValue={selectedSort}
+          handleSort={(value) => handleSortChange(value)}
         />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
