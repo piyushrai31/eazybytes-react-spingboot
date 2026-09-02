4286fc233281e2a405663622a49063fbe6047317
Piyush Rai <piyushraivns31@gmail.com>
2026-09-02 12:36:14 +0530

    React Router library implementation- Outleft, link, navlink, error page

---
+++ section10/eazystore-ui/src/main.jsx
@@ -0,0 +1,55 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+import Login from './components/Login.jsx';
+import Cart from './components/Cart.jsx';
+import Header from './components/Header.jsx';
+import Home from './components/Home.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "/home",
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      }
+
+    ]
+  }
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={appRouter} />
+  </StrictMode>
+);
