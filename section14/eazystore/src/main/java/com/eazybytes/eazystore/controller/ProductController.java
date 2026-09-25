499f7223359cb725d9ea15ac004876644e4002eb
Piyush Rai <piyushraivns31@gmail.com>
2026-09-25 18:02:36 +0530

    profile update implemented successfully

--- section14/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section14/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,36 +1,37 @@
 package com.eazybytes.eazystore.controller;
 
+import com.eazybytes.eazystore.dto.ErrorResponseDto;
 import com.eazybytes.eazystore.dto.ProductDto;
-import com.eazybytes.eazystore.entity.Product;
-import com.eazybytes.eazystore.repository.ProductRepository;
 import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("api/v1/products")
 @RequiredArgsConstructor
-//@CrossOrigin(origins = "http://localhost:5173/")
-@Slf4j
 public class ProductController {
 
     private final IProductService iProductService;
 
-
     @GetMapping
-    public ResponseEntity<List<ProductDto>> getProducts() throws InterruptedException {
-
+    public ResponseEntity<List<ProductDto>> getProducts() {
         List<ProductDto> productList = iProductService.getProducts();
-//        log.info("product details {}",productList.stream().collect(Collec));
-//        throw new RuntimeException("Error happened try again!");
-        return ResponseEntity.ok(productList);
+        return ResponseEntity.ok().body(productList);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                  WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false), HttpStatus.SERVICE_UNAVAILABLE,
+                exception.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.SERVICE_UNAVAILABLE);
     }
 
 }
